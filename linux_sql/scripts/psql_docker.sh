8f16c9f71fea034810ced5318a56471ba0529b06
Dat Vo <dathuuvo@gmail.com>
2021-07-07 11:53:53 -0400

    formatting and comment changes

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -4,13 +4,14 @@
 cmd=$1
 db_username=$2
 db_password=$3
-usage="./psql_docker.sh start|stop|create [db_username][db_password]"
+usage="./psql_docker.sh start|stop|create [db_username] [db_password]"
 
 # check if docker is running
-check_docker_status (){
-    sudo systemctl status --no-pager docker || systemctl start docker 
+check_docker_status() {
+    sudo systemctl status --no-pager docker || systemctl start docker
 }
 
+# check number of args
 if [ $# -lt 1 ]; then
     echo "no arguments supplied"
     echo $usage
@@ -18,6 +19,7 @@ if [ $# -lt 1 ]; then
 fi
 
 case $cmd in
+# create psql instance
 create)
     check_docker_status
     # check if containter created
@@ -26,20 +28,21 @@ create)
         echo ${usage}
         exit 1
     fi
-    
+
     # check username and password entered
     if [ -z $db_username ] || [ -z $db_password ]; then
         echo "db_username or db_password not supplied"
         echo ${usage}
         exit 1
     fi
-    
+
     # create persistant and psql container
     docker volume create pgdata
     docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_password} -e POSTGRES_USER=${db_username} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
     exit $?
     ;;
 
+# start the container
 start)
     check_docker_status
     docker container start jrvs-psql
@@ -47,6 +50,7 @@ start)
     exit $?
     ;;
 
+# stop running container
 stop)
     check_docker_status
     docker container stop jrvs-psql
@@ -54,6 +58,7 @@ stop)
     exit $?
     ;;
 
+# catch invalid commands
 *)
     echo "invalid command"
     echo $usage
